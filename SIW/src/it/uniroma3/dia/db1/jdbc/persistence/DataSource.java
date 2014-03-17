91c2e64b126bf8765da82444f81c278476d4a011
sara <saraformica@gmail.com>
2014-03-17 13:24:33 +0100

    w

---
+++ SIW/src/it/uniroma3/dia/db1/jdbc/persistence/DataSource.java
@@ -0,0 +1,21 @@
+package it.uniroma3.dia.db1.jdbc.persistence;
+
+import java.sql.*;
+
+public class DataSource {
+	private String dbURI = "jdbc:postgresql://localhost:5431/university";
+	private String user = "postgres";
+	private String password = "siw";
+
+	public Connection getConnection()  {
+		Connection connection = null;
+		try {
+			Class.forName("org.postgresql.Driver");
+			connection = DriverManager.getConnection(dbURI,user, password);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return connection;
+	}
+}
